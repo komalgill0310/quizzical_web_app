d6094dc28ca4ba04451274a3021a69364c36c793
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-07-25 01:02:22 -0400

    created a component folder and then created a file for the QuizIntroSetupPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React from "react";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+import QuizIntroSetupPage from "./components/QuizIntroSetupPage";
 
+function App() {
   return (
     <>
-      <h1>Quizzical</h1>
+      <QuizIntroSetupPage />
     </>
   );
 }
---
+++ src/components/QuizIntroSetupPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function QuizIntroSetupPage() {
+  return (
+    <div className="quiz-setup-container">
+      <h1>Quizzical</h1>
+    </div>
+  );
+}

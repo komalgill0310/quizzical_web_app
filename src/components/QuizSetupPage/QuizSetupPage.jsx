44ef6c32153fe45131db02874f45ab84fad5b229
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-09-06 20:17:05 -0400

    1. gave common class names to the select tags 2. added layout for container for QuizSetupPage component.

--- src/components/QuizSetupPage/QuizSetupPage.jsx
+++ src/components/QuizSetupPage/QuizSetupPage.jsx
@@ -44,40 +44,42 @@ export default function QuizSetupPage(props) {
       </p>
 
       {/* Below HTML for quiz topic */}
-      <label htmlFor="category-select">Select Category: </label>
-      <select
-        name="category"
-        id="category-select"
-        onChange={handleChange}
-        defaultValue="9"
-        className={styles.category}
-      >
-        <option value="any category">Any Category</option>
-        <option value="9">General Knowledge</option>
-        <option value="10">Entertainment: Books</option>
-        <option value="11">Entertainment: Film</option>
-        <option value="12">Entertainment: Music</option>
-        <option value="13">Entertainment: Musicals & Theatres</option>
-        <option value="14">Entertainment: Television</option>
-        <option value="15">Entertainment: Video Games</option>
-        <option value="16">Entertainment: Board Games</option>
-        <option value="17">Science & Nature</option>
-        <option value="18">Science: Computers</option>
-        <option value="19">Science: Mathematics</option>
-        <option value="20">Mythology</option>
-        <option value="21">Sports</option>
-        <option value="22">Geography</option>
-        <option value="23">History</option>
-        <option value="24">Politics</option>
-        <option value="25">Art</option>
-        <option value="26">Celebrities</option>
-        <option value="27">Animals</option>
-        <option value="28">Vehicles</option>
-        <option value="29">Entertainment: Comics</option>
-        <option value="30">Science: Gadgets</option>
-        <option value="31">Entertainment: Japanese Anime & Manga</option>
-        <option value="32">Entertainment: Cartoon & Animations</option>
-      </select>
+      <div className={styles.categoryContainer}>
+        <label htmlFor="category-select">Category </label>
+        <select
+          name="category"
+          id="category-select"
+          onChange={handleChange}
+          defaultValue="9"
+          className={`${styles.dropdownLabel} ${styles.category}`}
+        >
+          <option value="any category">Any Category</option>
+          <option value="9">General Knowledge</option>
+          <option value="10">Entertainment: Books</option>
+          <option value="11">Entertainment: Film</option>
+          <option value="12">Entertainment: Music</option>
+          <option value="13">Entertainment: Musicals & Theatres</option>
+          <option value="14">Entertainment: Television</option>
+          <option value="15">Entertainment: Video Games</option>
+          <option value="16">Entertainment: Board Games</option>
+          <option value="17">Science & Nature</option>
+          <option value="18">Science: Computers</option>
+          <option value="19">Science: Mathematics</option>
+          <option value="20">Mythology</option>
+          <option value="21">Sports</option>
+          <option value="22">Geography</option>
+          <option value="23">History</option>
+          <option value="24">Politics</option>
+          <option value="25">Art</option>
+          <option value="26">Celebrities</option>
+          <option value="27">Animals</option>
+          <option value="28">Vehicles</option>
+          <option value="29">Entertainment: Comics</option>
+          <option value="30">Science: Gadgets</option>
+          <option value="31">Entertainment: Japanese Anime & Manga</option>
+          <option value="32">Entertainment: Cartoon & Animations</option>
+        </select>
+      </div>
 
       {/* Below HTML for quiz difficulty level */}
       <div className={styles.difficultyContainer}>
